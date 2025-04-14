20934589043a1af37cfd1081630d33d179ae2164
illiaDubr <you@example.com>
2025-04-15 01:00:44 +0300

    initialaze project

---
+++ aqua-taxi/resources/js/components/App.vue
@@ -0,0 +1,13 @@
+<template>
+    <h1>Привет, Aqua Taxi!</h1>
+</template>
+
+<script setup>
+// пусто для теста
+</script>
+
+<style scoped>
+h1 {
+    color: #42b983;
+}
+</style>
---
+++ aqua-taxi/resources/views/welcome.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Aqua Taxi</title>
+    @vite('resources/js/app.js')
+</head>
+<body>
+<div id="app"></div>
+</body>
+</html>
