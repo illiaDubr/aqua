869bbc31cd70d99272ffc51a5a69a49eecf4e155
illiaDubr <you@example.com>
2025-05-22 15:30:49 +0300

    V1

--- aqua-taxi/vite.config.js
+++ aqua-taxi/vite.config.js
@@ -4,16 +4,13 @@ import path from 'path';
 
 export default defineConfig({
     plugins: [vue()],
-    resolve: {
-        alias: {
-            '@': path.resolve(__dirname, 'resources/js'),
-        },
-    },
+    root: 'resources/js', // путь к твоему JS (если у тебя так)
+    publicDir: 'public/vite-public', // если нужен (можно удалить)
     build: {
         manifest: true,
-        outDir: 'public/build',
+        outDir: path.resolve(__dirname, 'public/build'),
         rollupOptions: {
-            input: '/resources/js/app.js',
+            input: 'resources/js/app.js',
         },
         emptyOutDir: true,
     },
