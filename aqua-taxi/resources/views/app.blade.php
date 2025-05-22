e49c1469b1417c3e998274d2155534ed51f5d9e9
illiaDubr <you@example.com>
2025-05-22 16:18:34 +0300

    V1

--- aqua-taxi/resources/views/app.blade.php
+++ aqua-taxi/resources/views/app.blade.php
@@ -1,7 +1,3 @@
-@php
-    $manifest = json_decode(file_get_contents(public_path('build/manifest.json')), true);
-    $entry = $manifest['resources/js/app.js'] ?? null;
-@endphp
 
     <!DOCTYPE html>
 <html lang="en">
@@ -9,6 +5,10 @@
     <meta charset="UTF-8">
     <title>Aqua Taxi</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @php
+        $manifest = json_decode(file_get_contents(public_path('build/manifest.json')), true);
+        $entry = $manifest['resources/js/app.js'] ?? null;
+    @endphp
 
     @if ($entry && isset($entry['css']))
         @foreach ($entry['css'] as $css)
