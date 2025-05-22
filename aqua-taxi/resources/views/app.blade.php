c7a0de15805fa2ada48842795569d7555d42c95b
illiaDubr <you@example.com>
2025-05-22 15:25:32 +0300

    V1

--- aqua-taxi/resources/views/app.blade.php
+++ aqua-taxi/resources/views/app.blade.php
@@ -1,13 +1,19 @@
-<!DOCTYPE html>
+@php
+    $manifest = json_decode(file_get_contents(public_path('build/manifest.json')), true);
+@endphp
+
+    <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <title>Aqua Taxi</title>
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap" rel="stylesheet">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
-    @vite(['resources/js/app.js']) {{-- Подключение Vite в продакшене через manifest.json --}}
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="{{ asset('build/' . $manifest['resources/js/app.js']['css'][0]) }}">
 </head>
 <body class="antialiased">
 <div id="app"></div>
+
+<script type="module" src="{{ asset('build/' . $manifest['resources/js/app.js']['file']) }}"></script>
 </body>
 </html>
