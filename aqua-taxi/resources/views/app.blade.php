ba6fe008bcb648b59507b2e23ce2fa6462703ebd
illiaDubr <you@example.com>
2025-05-22 13:16:21 +0300

    V1

--- aqua-taxi/resources/views/app.blade.php
+++ aqua-taxi/resources/views/app.blade.php
@@ -3,16 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <title>Aqua Taxi</title>
-
-    @php
-        $manifest = json_decode(file_get_contents(public_path('build/manifest.json')), true);
-    @endphp
-
-    <link rel="stylesheet" href="{{ asset('build/' . $manifest['resources/js/app.js']['css'][0]) }}">
-    <script type="module" src="{{ asset('build/' . $manifest['resources/js/app.js']['file']) }}"></script>
-
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap" rel="stylesheet">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    @vite('resources/js/app.js')
 </head>
 <body>
 <div id="app"></div>
