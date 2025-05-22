d11e95022d0f3d31d9df2acf5e5870ecf7f38115
illiaDubr <you@example.com>
2025-05-22 12:40:20 +0300

    V1

--- aqua-taxi/vite.config.js
+++ aqua-taxi/vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    plugins: [vue()],
     build: {
         outDir: 'public/build',
         emptyOutDir: true,
