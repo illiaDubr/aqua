42260b3b773d486942adfead7605e4680cdaae87
illiaDubr <you@example.com>
2025-05-22 14:45:10 +0300

    V1

--- aqua-taxi/routes/web.php
+++ aqua-taxi/routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/{any}', function () {
-    return 'Laravel is alive';
+    return view('app'); // или welcome, если используешь welcome.blade.php
 })->where('any', '.*');
 Route::post('/auth/request-code', [AuthController::class, 'requestCode']);
 Route::post('/auth/verify-code', [AuthController::class, 'verifyCode']);
