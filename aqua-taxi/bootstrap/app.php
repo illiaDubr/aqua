0de002a9bd1d33b0f2810ed2b940b47c626a0623
illiaDubr <you@example.com>
2025-06-06 10:22:05 +0300

    v4

--- aqua-taxi/bootstrap/app.php
+++ aqua-taxi/bootstrap/app.php
@@ -6,7 +6,7 @@ use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withProviders([
-        Laravel\Reverb\ReverbServiceProvider::class,
+
     ])
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
