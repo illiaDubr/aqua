aff7add159124c6546a6081d6db02392944ec3d9
illiaDubr <you@example.com>
2025-10-01 04:12:42 +0300

    last pc

--- aqua-taxi/vite.config.js
+++ aqua-taxi/vite.config.js
@@ -1,19 +1,21 @@
 import { defineConfig } from 'vite'
-import laravel from 'laravel-vite-plugin'
 import vue from '@vitejs/plugin-vue'
+import laravel from 'laravel-vite-plugin'
 import path from 'path'
 
 export default defineConfig({
     plugins: [
-        laravel({ input: ['resources/js/app.js'], buildDirectory: 'build', refresh: false }),
+        laravel({
+            input: ['resources/js/app.js'],
+            buildDirectory: 'build', // => public/build
+            refresh: true,
+        }),
         vue(),
     ],
     resolve: { alias: { '@': path.resolve(__dirname, 'resources/js') } },
     build: {
         outDir: 'public/build',
         emptyOutDir: true,
-        manifest: true,
-        cssMinify: false,     // <-- временно отключаем минификацию CSS
+        manifest: true,           // кладёт manifest.json в public/build/
     },
-    server: { host: true, port: 5173 },
 })
