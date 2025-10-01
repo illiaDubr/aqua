241daf248093322112701b1d8f7986224cd6a8cd
Дубровін Ілля Олександрович <dubrovin.i@agromat.intranet>
2025-10-01 10:15:20 +0300

    water types

--- aqua-taxi/vite.config.js
+++ aqua-taxi/vite.config.js
@@ -1,13 +1,24 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import vue from '@vitejs/plugin-vue';
+import { defineConfig } from 'vite'
+
+import laravel from 'laravel-vite-plugin'
+import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 export default defineConfig({
     plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
+        laravel({ input: ['resources/js/app.js'], buildDirectory: 'build', refresh: false }),
+
+
+
+
         vue(),
     ],
-});
+    resolve: { alias: { '@': path.resolve(__dirname, 'resources/js') } },
+    build: {
+        outDir: 'public/build',
+        emptyOutDir: true,
+        manifest: true,
+        cssMinify: false,     // <-- временно отключаем минификацию CSS
+    },
+    server: { host: true, port: 5173 },
+})
