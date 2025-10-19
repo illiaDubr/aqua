33030d5e5de0c34c587e6f5653ce026c710f0262
illiaDubr <you@example.com>
2025-10-19 20:49:35 +0300

    modelF

--- aqua-taxi/app/Models/Factory.php
+++ aqua-taxi/app/Models/Factory.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Storage;
 
 class Factory extends Authenticatable
 {
@@ -24,6 +25,21 @@ class Factory extends Authenticatable
         'water_types' => 'array',
         'is_verified' => 'boolean',
         'verified_until' => 'datetime',
-        'lat' => 'float', 'lng' => 'float',
+        'lat' => 'float',
+        'lng' => 'float',
     ];
+
+    // ✅ Добавляем virtual-атрибут, который появится в JSON
+    protected $appends = ['certificate_url'];
+
+    // ✅ Генерация корректного публичного URL
+    public function getCertificateUrlAttribute(): ?string
+    {
+        if (!$this->certificate_path) {
+            return null;
+        }
+
+        // вернёт что-то вроде: https://aquataxi.online/storage/certificates/abc123.jpg
+        return Storage::disk('public')->url($this->certificate_path);
+    }
 }
